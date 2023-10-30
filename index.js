566aa473655a65fe866cc27ff3b830c4246d9eaa
Emirhan Yaman <90368997+yamanemirhan@users.noreply.github.com>
2023-10-30 23:28:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Header
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://socketio-chatting.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
